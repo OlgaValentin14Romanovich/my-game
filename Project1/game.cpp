accbd235fc386f5eeff4a98c824c2d02baf01e6f
OlgaValentin14Romanovich <153107779+OlgaValentin14Romanovich@users.noreply.github.com>
2023-12-29 20:49:13 +0300

    добавлен 4-ый прямоугольник

--- Project1/game.cpp
+++ Project1/game.cpp
@@ -8,13 +8,15 @@ float playerPositionY = 0.f;
 internalVariable void simulateGame(Input* input) {
 	clearScreen(0xff5500);
 	
-
+	
+	
 	if (pressed(BUTTON_UP)) playerPositionY += 1.f;
 	if (pressed(BUTTON_DOWN)) playerPositionY -= 1.f;
 	if (pressed(BUTTON_LEFT)) playerPositionX -= 1.f;
 	if (pressed(BUTTON_RIGHT)) playerPositionX += 1.f;
 	
 	drawRectangle(playerPositionX, playerPositionY, 1, 1, 0x000000);
+	drawRectangle(15+playerPositionX,15+ playerPositionY, 8, 1, 0x00FF00);
 	drawRectangle(30+playerPositionX, 30+ playerPositionY, 5, 5, 0x008080);
 	drawRectangle(-20+ playerPositionX, 20+ playerPositionY, 8, 3, 0x000080);
 }
