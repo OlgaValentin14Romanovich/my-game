40863be14e6c965e97481357058fb58ce13f9d39
OlgaValentin14Romanovich <153107779+OlgaValentin14Romanovich@users.noreply.github.com>
2023-12-29 20:33:40 +0300

    изменен цвет прямоугольников и все фигуры приходят в движение .

--- Project1/game.cpp
+++ Project1/game.cpp
@@ -7,12 +7,15 @@ float playerPositionY = 0.f;
 
 internalVariable void simulateGame(Input* input) {
 	clearScreen(0xff5500);
+	
+	
+	
 	if (pressed(BUTTON_UP)) playerPositionY += 1.f;
 	if (pressed(BUTTON_DOWN)) playerPositionY -= 1.f;
 	if (pressed(BUTTON_LEFT)) playerPositionX -= 1.f;
 	if (pressed(BUTTON_RIGHT)) playerPositionX += 1.f;
 	
-	drawRectangle(playerPositionX, playerPositionY, 1, 1, 0x00ff22);
-	drawRectangle(30, 30, 5, 5, 0xffff22);
-	drawRectangle(-20, 20, 8, 3, 0xffff22);
+	drawRectangle(playerPositionX, playerPositionY, 1, 1, 0x000000);
+	drawRectangle(30+playerPositionX, 30+ playerPositionY, 5, 5, 0x008080);
+	drawRectangle(-20+ playerPositionX, 20+ playerPositionY, 8, 3, 0x000080);
 }
